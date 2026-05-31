18621ab5927fd238a04233a256d94e4961df40c4
postive <postive12@gmail.com>
2026-05-31 12:28:55 +0900

    chore: version up to 2.0.15

--- Scripts/Editor/ODDBEditorRuntime.cs
+++ Scripts/Editor/ODDBEditorRuntime.cs
@@ -181,7 +181,7 @@ Workflow shortcuts you can offer the user:
             _dispatcher.Register("initialize", (id, p) => McpResponse.Success(id, new
             {
                 protocolVersion = "2024-11-05",
-                serverInfo = new { name = "ODDB", version = "2.0.14" },
+                serverInfo = new { name = "ODDB", version = "2.0.15" },
                 capabilities = new { tools = new { }, resources = new { } },
                 instructions = ServerInstructions,
             }));
