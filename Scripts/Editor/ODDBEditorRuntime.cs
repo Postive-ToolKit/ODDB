4f06cc8e9311eb44ee45b77a5a9273b799d3f53d
postive12 <postive12@gmail.com>
2026-07-05 15:06:29 +0900

    fix: include Unity package metadata

--- Scripts/Editor/ODDBEditorRuntime.cs
+++ Scripts/Editor/ODDBEditorRuntime.cs
@@ -189,7 +189,7 @@ Workflow shortcuts you can offer the user:
             _dispatcher.Register("initialize", (id, p) => McpResponse.Success(id, new
             {
                 protocolVersion = "2024-11-05",
-                serverInfo = new { name = "ODDB", version = "2.2.2" },
+                serverInfo = new { name = "ODDB", version = "2.2.3" },
                 capabilities = new { tools = new { }, resources = new { } },
                 instructions = ServerInstructions,
             }));
